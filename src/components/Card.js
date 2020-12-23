5c00617704e33c37fb31ed4f8bcbb9e2d58c3c7a
Ngozi Ekekwe <ngozi-ekekwe>
2020-12-23 23:32:40 +0100

    trim text

--- src/components/Card.js
+++
@@ -1,5 +0,0 @@
-export default function Card(params) {
-  return (
-    <div>This is a card</div>
-  )
-} 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,9 +1,21 @@
 import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { useRef, useEffect } from "react";
 
 export default function Hero({ movie }) {
-  console.log(movie, 'moviemovie')
   const moviePath = movie?.backdrop_path || movie?.poster_path;
   const image = moviePath && `https://image.tmdb.org/t/p/original/${moviePath}`;
+
+  const movieOverview = useRef(null);
+  let divHeight = movieOverview.current && movieOverview.current.offsetHeight;
+  
+  useEffect(()=> {
+    if(divHeight > 179) {
+      movieOverview.current.style.maxHeight = '60px';
+      movieOverview.current.style.height = '60px';
+      movieOverview.current.style.overflow = 'hidden';
+    }
+  }, [divHeight])
+
   return (
     <div
       className="hero"
@@ -18,7 +30,9 @@ export default function Hero({ movie }) {
                 <h2 className="title">
                   {movie.title || <Skeleton height={20} width={300} />}
                 </h2>
-                <p>{movie.overview || <Skeleton duration={2} />}</p>
+                <p ref={movieOverview}>
+                  {movie.overview || <Skeleton duration={2} />}
+                </p>
               </SkeletonTheme>
               <div className="actions">
                 <button className="play">Watch Trailer</button>
